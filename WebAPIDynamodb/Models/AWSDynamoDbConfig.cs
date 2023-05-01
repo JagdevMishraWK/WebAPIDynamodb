afc5f6eb61773cbb212f1057e936301c3d73884d
Jagdev Mishra <Jagdev.Mishra@wolterskluwer.com>
2023-05-01 11:56:38 +0530

    Dynamobd code

---
+++ WebAPIDynamodb/Models/AWSDynamoDbConfig.cs
@@ -0,0 +1,12 @@
+﻿namespace WebAPIDynamodb.Models
+{
+    public class AWSDynamoDbConfig
+    {
+        public string Access_Key { get; set; }
+        public string Secret_Key { get; set; }
+        public string Region { get; set; }
+        public string LocalServiceUrl { get; set; }
+        public string TableNamePrefix { get; set; }
+        public  bool LocalMode { get; set; }
+    }
+}
--- WebAPIDynamodb/Program.cs
+++ WebAPIDynamodb/Program.cs
@@ -1,32 +1,60 @@
+using Amazon.DynamoDBv2.DataModel;
+using Amazon.DynamoDBv2;
+using Amazon.Runtime;
+using Amazon;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using WebAPIDynamodb.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddControllers();
+builder.Services.AddSwaggerGen();
+
+// Configure AWS service clients to use these credentials
+var dynamoDbConfig = builder.Configuration.GetSection("AWSDynamoDbConfig");
+var accessKey = dynamoDbConfig.GetValue<string>("Access_Key");
+var secretKey = dynamoDbConfig.GetValue<string>("Secret_Key");
+var region = dynamoDbConfig.GetValue<string>("Region");
+var runLocalDynamoDb = dynamoDbConfig.GetValue<bool>("LocalMode");
+var localServiceUrl = dynamoDbConfig.GetValue<string>("LocalServiceUrl");
+var credentials = new BasicAWSCredentials(accessKey, secretKey);
+
+//Method-1
+var config = new AmazonDynamoDBConfig()
+{
+    AuthenticationRegion = region
+};
+if (runLocalDynamoDb)
+{
+    //Method-2
+    config.ServiceURL = localServiceUrl;
+}
+
+var dynamoDbClient = new AmazonDynamoDBClient(credentials, config);
+builder.Services.AddSingleton<IAmazonDynamoDB>(dynamoDbClient);
+var context = new DynamoDBContext(dynamoDbClient);
+builder.Services.AddSingleton<IDynamoDBContext>(context);
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-
-var summaries = new[]
+if (app.Environment.IsDevelopment())
 {
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "AWS: WebAPI");
+    });
+}
 
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-});
+
+//Cognito Configuration: 
+app.UseAuthentication();
+
+app.UseAuthorization();
+
+app.MapControllers();
 
 app.Run();
-
-internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
